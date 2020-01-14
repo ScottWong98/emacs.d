eab7aeb3a431a4096e5149b7d4fec09d598c0a45
scott <527407973@qq.com>
2020-01-14 19:49:13 +0800

    add web-mode

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -7,19 +7,20 @@
 
 
 (defvar my/packages '(
-			 company
-			 monokai-theme
-			 solarized-theme
-			 nord-theme
-			 dracula-theme
-			 hungry-delete
-			 swiper
-			 counsel
-			 smartparens
-			 js2-mode
-			 nodejs-repl
-			 popwin
-			 ) "Default packages")
+		      company
+		      monokai-theme
+		      solarized-theme
+		      nord-theme
+		      dracula-theme
+		      hungry-delete
+		      swiper
+		      counsel
+		      smartparens
+		      js2-mode
+		      nodejs-repl
+		      popwin
+		      web-mode
+		      ) "Default packages")
 
 (require 'cl)
 (setq package-selected-packages my/packages)
@@ -65,7 +66,8 @@
 ;; js2-mode
 (setq auto-mode-alist
       (append
-       '(("\\.js\\'" . js2-mode))
+       '(("\\.js\\'" . js2-mode)
+	 ("\\.html\\'" . web-mode))
        auto-mode-alist))
 
 ;; Nodejs
@@ -79,5 +81,34 @@
 ;; Popwin
 (require 'popwin)
 (popwin-mode t)
+
+;; Web-mode
+(defun my-web-mode-indent-setup ()
+  (setq web-mode-markup-indent-offset 2) ; web-mode, html tag in html file
+  (setq web-mode-css-indent-offset 2)    ; web-mode, css in html file
+  (setq web-mode-code-indent-offset 2)   ; web-mode, js code in html file
+  )
+(add-hook 'web-mode-hook 'my-web-mode-indent-setup)
+;; toggle web indent (2 <-> 4)
+(defun my-toggle-web-indent ()
+  (interactive)
+  ;; web development
+  (if (or (eq major-mode 'js-mode) (eq major-mode 'js2-mode))
+      (progn
+	(setq js-indent-level (if (= js-indent-level 2) 4 2))
+	(setq js2-basic-offset (if (= js2-basic-offset 2) 4 2))))
+
+  (if (eq major-mode 'web-mode)
+      (progn (setq web-mode-markup-indent-offset (if (= web-mode-markup-indent-offset 2) 4 2))
+	     (setq web-mode-css-indent-offset (if (= web-mode-css-indent-offset 2) 4 2))
+	     (setq web-mode-code-indent-offset (if (= web-mode-code-indent-offset 2) 4 2))))
+  (if (eq major-mode 'css-mode)
+      (setq css-indent-offset (if (= css-indent-offset 2) 4 2)))
+
+  (setq indent-tabs-mode nil))
+
+(global-set-key (kbd "C-c t i") 'my-toggle-web-indent)
+
+
 
 (provide 'init-packages)
